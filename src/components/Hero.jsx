a06ec8a8d13476c941f6637a12bb7cf1bf450a70
mandubaritoo <jotelui14@gmail.com>
2026-07-14 02:06:28 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,31 @@
+import bannerImg from "../assets/banner.png";
+
 function Hero({ saludo }) {
   return (
     <section className="hero" id="inicio">
       <div className="contenedor hero-contenido">
         <div className="hero-texto">
-          <p className="etiqueta">{saludo}, soy estudiante de Analista Programador</p>
+          <p className="etiqueta">
+            {saludo}, soy estudiante de Analista Programador
+          </p>
           <h2>Bienvenido a mi landing page en React</h2>
           <p>
             Soy estudiante de Analista Programador en INACAP. Esta aplicación
             será la evolución de mi landing page anterior, ahora construida en
             React para mostrar información útil, validaciones y manejo de datos.
           </p>
-          <a href="#proyecto" className="boton">Ver proyecto</a>
+          <a href="#proyecto" className="boton">
+            Ver proyecto
+          </a>
         </div>
 
         <div className="hero-imagen">
           <div className="hero-placeholder">
-            <span>Vista principal del proyecto</span>
+            <img
+              src={bannerImg}
+              alt="Vista principal del proyecto"
+              className="hero-imagen-foto"
+            />
           </div>
         </div>
       </div>
